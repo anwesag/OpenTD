052f39d1ac16f7cc612347d0c69542bfc9cab990
anwesag2 <anwesag2@illinois.edu>
2020-06-07 19:18:59 -0700

    sameteam function added

--- definitions.js
+++ definitions.js
@@ -7,8 +7,8 @@ let player = {
   rating: 1849,
   points: 0,
   color: 0,
-  played: [] // game objects,
-  //teamnum: 0   //not sure where to access team information from
+  played: [], // game objects,
+  teams: []   //not sure where to access team information from
 }
 
 
@@ -34,9 +34,15 @@ function played (player1, player2) {
   return false
 }
 
-// function sameteam (player1, player2){  //potential same-team-checker
-//    return (player1.teamnum == player2.teamnum)
-// }
+function sameteam (player1, player2){  //potential same-team-checker
+  for(let i = 0; i < player1.teams.length; i++){
+    for(let j = 0; j < player2.teams.length; j++){
+      if(player1.teams[i] === player2.teams[j]){
+        return true
+      }
+    }
+    return false
+}
 
 function colorcheck (player1, player2){  //same-color-checker
   return (player1.color * player2.color <= 0)

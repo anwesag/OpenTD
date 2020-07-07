5603f98d12ce41237fbb486e17d8f2f836e9c991
MBafna <reachmihirbafna@gmail.com>
2020-07-06 21:33:43 -0500

    rearranged

--- Calculations/Weights.js
+++ Calculations/Weights.js
@@ -12,24 +12,11 @@ const Games = require('../../Base/games.js')
 //Rule 1: Avoid meeting players twice. Subtract 100 everytime played this player 1 or more times
 const rule_1 = 100
 
-function rule_1_already_played(player1, player2) {
-  weight = 0
-  for (var game in player1.played) {
-    if(game.id_white === player2.id || game.id_black === player2.id){
-      weight -= rule_1
-    }
-  }
-  return weight
-}
 /* Rule 2: Ideally play people with equal scores. Subtract 10 points for each difference in points
 *  Ex: Player 1 (4.0) vs Player 2 (4.0) -> 40 points
 *  Ex: Player 1 (4.0) vs PLayer 2 (2.0) -> 20 points
 */
 const rule_2 = 40
-
-function rule_2_similar_scores(player1, player2){
-  return rule_2 - (10 * Math.abs(player1.points - player2.points))
-}
 
 /* Rule 3: Upper half vs Lower Half: Based on middle determined by helper function, determine how ideal of a placement a player is at.
 *  Score decreases based on distance from middle
@@ -40,13 +27,33 @@ const rule_3 = 15
 */
 const rule_4 = 5
 
+/* Rule 5: Alternate colors: Play someone so the color you play is different from last round. Not added if does not happen.
+*/
+const rule_5 = 1
+
+
+function rule_1_already_played(player1, player2) {
+  weight = 0
+  for (var game in player1.played) {
+    if(game.id_white === player2.id || game.id_black === player2.id){
+      weight -= rule_1
+    }
+  }
+  return weight
+}
+
+
+function rule_2_similar_scores(player1, player2){
+  return rule_2 - (10 * Math.abs(player1.points - player2.points))
+}
+
+
+
 function rule_4_equalizing_colors(player1, color_player_1) {
   return rule_4 - Math.abs(player1.color)
 }
 
-/* Rule 5: Alternate colors: Play someone so the color you play is different from last round. Not added if does not happen.
-*/
-const rule_5 = 1
+
 
 function rule_5_alternate_colors(player1, color_player_1) {
   //Check if color matches from previous game

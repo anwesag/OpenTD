22360d731dfd3dd8dc457e5bc44f73a7aa285f57
anwesag2 <anwesag2@illinois.edu>
2020-07-06 19:25:32 -0700

    Helper functions for rules 1 and 2

--- Calculations/Weights.js
+++ Calculations/Weights.js
@@ -9,12 +9,24 @@
 //Rule 1: Avoid meeting players twice. Subtract 100 everytime played this player 1 or more times
 const rule_1 = 100
 
+function already_played(player1, player2) {
+  weight = 0
+  for game in player1.played {
+    if(game.id_white === player2.id || game.id_black === player2.id){
+      weight -= rule_1
+    }
+  }
+  return weight
+}
 /* Rule 2: Ideally play people with equal scores. Subtract 10 points for each difference in points
 *  Ex: Player 1 (4.0) vs Player 2 (4.0) -> 40 points
 *  Ex: Player 1 (4.0) vs PLayer 2 (2.0) -> 20 points
 */
 const rule_2 = 40
 
+function similar_scores(player1, player2){
+  return rule_2 - (10 * Math.abs(player1.points - player2.points))
+}
 
 /* Rule 3: Upper half vs Lower Half: Based on middle determined by helper function, determine how ideal of a placement a player is at.
 *  Score decreases based on distance from middle

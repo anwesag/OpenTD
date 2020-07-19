643d35ad5e949b67ea31beb043b4c2a9d4b2749f
MBafna <reachmihirbafna@gmail.com>
2020-07-18 20:36:46 -0500

    comments

---
+++ Calculations/round.js
@@ -0,0 +1,10 @@
+//This will run an entire round and create the games needed
+
+
+/*
+* We need to seperate out the array into categories based on points
+* We need to make sure we have an even number of Players
+* If not, we need to take out the lowest ranking person without a bye
+* Adjust arrays so every array has an even amount of people
+* Run rules for everyone
+*/
